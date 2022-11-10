bb3e66c434ac1edd99b426419211ddca58588990
vision <karohambaryan@gmail.com>
2022-11-10 13:11:36 +0400

    todo-with-types

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import { Task } from './types/Task'
+import ToDoList from './components/ToDoList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App:React.FC = () => {
+  const [todos, setTodos] = useState<Task[]>([])
+  const [done, setDone] = useState<number>(0)
+
+  useEffect(() => {
+    setDone(todos.filter(a => a.done).length)
+  }, [todos])
+
+  const addTask = (task:Task):void => {
+    setTodos([task,...todos])
+  }
+
+  const completeTask = (task:Task):void => {
+    task.done = true
+    task.timeCompleted = Date.now()
+    setTodos([...todos])
+  }
+
+  const cancelTask = (task:Task):void => {
+    task.done = false
+    task.timeCompleted = null
+    setTodos([...todos])
+  }
+
+  return <div>
+    <h3>TO DO LIST</h3>
+    <ToDoList
+      tasks = {todos}
+      done = {done}
+      onAdd ={addTask}
+      onComplete = {completeTask}
+      onCancel = {cancelTask}
+    />
+  </div>
 }
 
 export default App;
